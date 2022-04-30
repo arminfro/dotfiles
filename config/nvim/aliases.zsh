05f0796af3bd83e4a5ed6704e28a4134ceb35849
Armin Fröhlich <mail@arminfroehlich.de>
2022-04-30 12:22:49 +0200

    add zettelkasten support by zk-nvim
    add alias for vim-logs
    make fzf ripgrep ignoring specific folders

--- config/nvim/aliases.zsh
+++ config/nvim/aliases.zsh
@@ -1,5 +1,7 @@
 alias vim='nvim'
 alias v='vim'
 
+alias vim-logs='vim ~/.cache/nvim/*.log'
+
 alias vim-update-plugins="nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'"
 alias v-update-plugins="vim-update-plugins"
