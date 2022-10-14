15dd79f4cc2d1443c06373f6a52496e8af9a1293
Armin Fröhlich <mail@arminfroehlich.de>
2022-10-14 07:01:13 +0200

    update neovim config

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -14,6 +14,11 @@ export SPACESHIP_PACKAGE_COLOR="161"
 export SPACESHIP_VI_MODE_NORMAL="[]"
 export SPACESHIP_VI_MODE_INSERT="[]"
 
+# https://github.com/spaceship-prompt/spaceship-prompt/issues/1193
+# export SPACESHIP_PROMPT_ASYNC=false
+# export SPACESHIP_GIT_BRANCH_SHOW=true
+# export SPACESHIP_TIME_SHOW=true
+
 # load zgenom
 source "${HOME}/.zgenom/zgenom.zsh"
 
