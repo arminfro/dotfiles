11f5514c92b3e5aaae1648e66f8185d9d105cdf3
Armin Fröhlich <mail@arminfroehlich.de>
2023-05-23 07:55:14 +0200

    update packages, to install dep for neovim

--- config/nvim/install.sh
+++ config/nvim/install.sh
@@ -1,4 +1,2 @@
 # setup yarn config, should be done in yarn/install.sh, but then execution order matters
 yarn config set prefix ~/.yarn
-
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
