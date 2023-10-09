4274d8df892cc45f6cb1b19935e605bc2bfb64eb
Armin Fröhlich <mail@arminfroehlich.de>
2023-10-09 17:56:09 +0200

    fix keymaps for new lazyvim version

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,8 +1,6 @@
 # Pipe my public key to my clipboard.
 alias pubkey="xclip -selection clipboard ~/.ssh/id_rsa.pub && echo '=> Public key copied by xclip.'"
 
-alias journalfollow="journalctl -b -f | ccze -A -o nolookups"
-
 alias feh="feh -ZxF"
 
 alias cat="bat --color=always --number"
