4c73435cf4f457d7190c23ac53d8c081ff6ed8b6
Armin Fröhlich <mail@arminfroehlich.de>
2023-10-12 19:34:15 +0200

    update nvim

--- config/nvim/aliases.zsh
+++ config/nvim/aliases.zsh
@@ -1,8 +1,5 @@
-alias nvim='SHELL=/bin/sh nvim'
-alias vim='SHELL=/bin/sh nvim'
-alias v='SHELL=/bin/sh nvim'
+alias nvim='nvim'
+alias vim='nvim'
+alias v='nvim'
 
 alias vim-logs='vim ~/.cache/nvim/*.log'
-
-alias vim-update-plugins="nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'"
-alias v-update-plugins="vim-update-plugins"
